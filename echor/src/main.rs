854f91b6cec53f842339f18af54e7843375967c8
Michael B. Espeña <michael.esp42@pm.me>
2022-09-22 15:49:22 +0800

    create new `App`
    - import `clap::App` struct
    - create a new `App` called `echor`
    - add (semantic) version number
    - include author information
    - add `App` description

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("Michael Espeña <michael.esp42@pm.me>")
+        .about("Rust echo")
+        .get_matches();
 }

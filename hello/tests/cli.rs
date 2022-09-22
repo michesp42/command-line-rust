6f64b4c9f28e3be96b28ba9cbbe6746960aeac41
Michael B. Espeña <michael.esp42@pm.me>
2022-09-22 09:41:07 +0800

    specify the full path to `hello` in the test
    - this will result in a successful test

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::new("hello");
+    let mut cmd = Command::new("target/debug/hello");
     let res = cmd.output();
     assert!(res.is_ok());
 }

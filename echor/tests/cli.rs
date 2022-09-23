9f7e5fb4efb90a178b9da45e42edad1fd424a4fb
Michael B. Espeña <michael.esp42@pm.me>
2022-09-23 20:01:19 +0800

    use `TestResult`
    use custom `Result` type to handle errors

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -3,12 +3,15 @@ use std::fs;
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]

ffd8866edb53c1148fbefae890a1690dfc9d7df4
Michael B. Espeña <michael.esp42@pm.me>
2022-09-23 19:36:08 +0800

    use `contains_id`
    `is_present` is decprecated use `contains_id` instead

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
         )
         .get_matches();
     let text = matches.values_of_lossy("text").unwrap();
-    let omit_newline = matches.is_present("omit_newline");
+    let omit_newline = matches.contains_id("omit_newline");
 
     println!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }

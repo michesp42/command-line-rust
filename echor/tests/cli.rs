58b2c96d8d76b7a13c7721a467b5df5f6af7d3c9
Michael B. Espeña <michael.esp42@pm.me>
2022-09-23 20:02:39 +0800

    add test for `hello1.txt`

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -15,7 +15,9 @@ fn dies_no_args() -> TestResult {
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
+fn hello1() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
